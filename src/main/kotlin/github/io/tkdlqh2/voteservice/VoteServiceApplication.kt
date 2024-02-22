4ab71a1dd0ff5f334ea86ff2f67aac6bdf4e0d37
Hyeongjin <yhj7124@kusrc.co.kr>
2024-02-22 15:28:40 +0900

    feat: 첫 커밋

---
+++ src/main/kotlin/github/io/tkdlqh2/voteservice/VoteServiceApplication.kt
@@ -0,0 +1,11 @@
+package github.io.tkdlqh2.voteservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoteServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<VoteServiceApplication>(*args)
+}
---
+++ src/test/kotlin/github/io/tkdlqh2/voteservice/VoteServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package github.io.tkdlqh2.voteservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoteServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
